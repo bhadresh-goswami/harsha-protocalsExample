8254b2b3fb2a62941dfe7d67e99280461d6f793b
tops-bhadresh <tops.bhadresh@gmail.com>
2019-01-08 05:27:23 -0800

    upload

--- protocalsExample/main.swift
+++ protocalsExample/main.swift
@@ -8,5 +8,52 @@
 
 import Foundation
 
-print("Hello, World!")
+class course {
+    var title:String = ""
+    var fees:Double = 0.0
+    
+    func SetValues(title:String,fees:Double)  {
+        self.title = title
+        self.fees = fees
+    }
+    
+    
+}
 
+@objc protocol CourseRules {
+    func Timing()
+    func Assignments()
+    @objc optional func Exam()
+}
+
+class Harsha:course, CourseRules {
+    func Timing() {
+        
+    }
+    
+    func Assignments() {
+        
+    }
+    
+    func showData()  {
+        print(title)
+        print(fees)
+    }
+    func Exam() {
+        
+    }
+}
+class Rishi:course, CourseRules {
+    func Timing() {
+        
+    }
+    
+    func Assignments() {
+        
+    }
+    
+    func showMyData()  {
+        print(title)
+        print(fees)
+    }
+}
