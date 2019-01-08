20c8cb5a5a524504bcaeb905cdaf6cf70a2f8d8a
tops-bhadresh <tops.bhadresh@gmail.com>
2019-01-08 05:18:05 -0800

    Initial Commit

---
+++ protocalsExample/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  protocalsExample
+//
+//  Created by Mac on 08/01/19.
+//  Copyright © 2019 bhadresh. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
